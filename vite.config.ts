3fa705ad1306dec3039fd3a30f637b46ba07aeb7
wintec-yuda <alamasta004@gmail.com>
2025-06-25 08:59:07 +0700

    Solve: error build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    base: env.VITE_BASE_PATH || '/',
+  }
 })
